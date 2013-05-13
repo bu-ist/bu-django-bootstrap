5bc51baec83adbd79af27c19b6c424a1da82e70e
Fyodor Wolf <fwolf@bu.edu>
2013-05-13 11:53:11 -0400

    added comments to the shell/configVenv.sh script

--- vagrant/shell/configVenv.sh
+++ vagrant/shell/configVenv.sh
@@ -1,6 +1,8 @@
+#create/activate the virtual environment once loaded...
 virtualenv /var/apps/djangoapp/venv/
 source /var/apps/djangoapp/venv/bin/activate
 
+#make sure to install the proper requirements files into the venv.
 if [ -f "/app/repo/requirements.txt" ]
 then
 	pip install -r /app/repo/requirements.txt
@@ -8,6 +10,7 @@ else
 	pip install -r /app/quick_start/templates/project_template/requirements.txt
 fi
 
+#append the following lines to .profile for expected login behavior
 #make sure to activate venv on login.
 grep -q "source /var/apps/djangoapp/venv/bin/activate" /home/vagrant/.profile || 
 echo "source /var/apps/djangoapp/venv/bin/activate" >> /home/vagrant/.profile
