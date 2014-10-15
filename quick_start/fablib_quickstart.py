7030802a4b1329d54431fa8b3f7d4167787a8eaa
Fyodor Wolf <fwolf@bu.edu>
2014-10-15 11:22:41 -0400

    updates for virtualenv support.

--- quick_start/fablib_quickstart.py
+++ quick_start/fablib_quickstart.py
@@ -4,13 +4,6 @@ from fabric.colors import red, green, yellow
 from sys import exit
 import os
 
-# This is BU's Fabric library.
-# It has generic deploy/setup tasks, including some optional handling
-# for AFS (both in terms of SCM locations and static media).
-# It should ultimately be an independent package, though currently it's
-# distributed with the BU Django Bootstrap package.
-
-
 def start(project=None, app=None):
     """
     Fast start the Django applicatin with the bare bone minimum. USAGE: $ fab start:project=project_name,app=app_name
@@ -23,7 +16,7 @@ def start(project=None, app=None):
 
     if files.exists(env.git_path):
         local('cd %s; git add -A; git commit -m "Initial Commit"' %
-              (env.app_path))
+              (env.repo_path))
         print green("Added and commited your project files to git.")
         print yellow("If you haven't so already, make sure to add a remote orgin with: $ git remote add origin https://PATH_TO_GITHUB_REPO.git; git remote -v", )
 
@@ -37,13 +30,13 @@ def start_project(project_name):
     script = env.venv_bin + "django-admin.py"
     command = "startproject"
     template = "/app/quick_start/templates/project_template"
-    destPath = env.app_path
+    destPath = env.repo_path
 
     local("python %s %s %s --template=%s %s" %
           (script, command, env.project_name, template, destPath))
     print green("Created new project '%s' in: %s" % (env.project_name, destPath))
 
-    local("cd %s; git init" % env.app_path)
+    local("cd %s; git init" % env.repo_path)
     print green("Initialized a new git repository in '%s'" % (destPath))
 
     install_requirements()
@@ -62,17 +55,17 @@ def start_app(app_name=None):
 
     script = env.venv_bin + "django-admin.py"
     command = "startapp"
-    template = "/app/quick_start/templates/app_template"
+    template = env.path+"/quick_start/templates/app_template"
 
     local("python %s %s %s --template=%s %s" %
           (script, command, app_name, template, destPath))
     print green("Started new app in: %s" % destPath)
 
     if(env.project_name != None):
-        settings = env.app_path + env.project_name + "/settings.py"
+        settings = env.repo_path + env.project_name + "/settings.py"
         line = "INSTALLED_APPS +=  ('apps.%s',)" % app_name
         local('echo "%s" >> %s' % (line, settings))
-        project_urls = env.app_path + env.project_name + "/urls.py"
+        project_urls = env.repo_path + env.project_name + "/urls.py"
         line = "urlpatterns += patterns('',url(r'', include('apps.%s.urls')),)" % app_name
         local('echo "%s" >> %s' % (line, project_urls))
         print green("Hooked app '%s' into project '%s'" % (app_name, env.project_name))
@@ -87,9 +80,9 @@ def pickup(project=None, do_requirements='yes', do_apache='yes'):
         exit(0)
     env.project_name = project
     do_sqlite = 'yes' if files.exists(
-        env.app_path + "sqlite/django.sqlite") and files.exists(env.git_path) else 'no'
+        env.repo_path + "sqlite/django.sqlite") and files.exists(env.git_path) else 'no'
     do_wrap_up = 'yes' if files.exists(
-        env.app_path + "sys_requirements.sh") else 'no'
+        env.repo_path + "sys_requirements.sh") else 'no'
 
     if do_requirements == 'yes':
         install_requirements()
@@ -98,17 +91,17 @@ def pickup(project=None, do_requirements='yes', do_apache='yes'):
     if do_sqlite == 'yes':
         local(
             'cd %s; git update-index --assume-unchanged sqlite/django.sqlite;' %
-            (env.app_path))
+            (env.repo_path))
         print green("Ignoring local sqlite file.")
         print yellow("Make sure the host's sqlite folder and contents have permission set to 777.")
     if do_wrap_up == 'yes':
-        local('sh %s;' % env.app_path + "sys_requirements.sh")
+        local('sh %s;' % env.repo_path + "sys_requirements.sh")
         print green("Installed additional sys_requirements.sh")
     print green("setup complete! visit http://localhost:8080 to see the site.")
 
 
 def install_requirements():
-    file_path = env.app_path + "requirements.txt"
+    file_path = env.repo_path + "requirements.txt"
     if not files.exists(file_path):
         print red('Unable to locate: %s') % (file_path)
         print yellow('To skip the installation, add the do_requirements="no" argument.')
